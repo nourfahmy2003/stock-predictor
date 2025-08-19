4e727676c00b2f14eff8f9f278a0e2bf47afd2b1
nourfahmy2003 <109670999+nourfahmy2003@users.noreply.github.com>
2025-08-19 22:17:26 +0300

    Remove unused prediction bridge components

--- frontend/app/t/[ticker]/layout.jsx
+++ frontend/app/t/[ticker]/layout.jsx
@@ -1,13 +1,3 @@
-import PredictionStatusBanner from "@/components/stock/PredictionStatusBanner"
-import TabBridge from "@/components/stock/TabBridge"
-
-export default function TickerLayout({ params, children }) {
-  const ticker = params.ticker?.toUpperCase()
-  return (
-    <>
-      <TabBridge ticker={ticker} />
-      <PredictionStatusBanner ticker={ticker} />
-      {children}
-    </>
-  )
+export default function TickerLayout({ children }) {
+  return <>{children}</>
 }
